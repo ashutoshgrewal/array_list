c0f05d120134296c4a5ae0165cbfd3256506871a
Ashutosh Grewal <ashutoshgrewal@gmail.com>
2016-12-17 16:40:24 -0800

    Initial Commit

---
+++ array_list/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  array_list
+//
+//  Created by Ashutosh Grewal on 12/17/16.
+//  Copyright © 2016 Ashutosh Grewal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
